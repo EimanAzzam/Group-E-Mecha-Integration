aa25ef7bc4db6d61008a44d46e7f866954ce98d5
muhdfahmiikhwan <148888144+muhdfahmiikhwan@users.noreply.github.com>
2023-11-07 22:58:05 +0800

    Task_4a_arduino

--- Week4/task_week4.py
+++ Week4/task_week4.py
@@ -4,10 +4,10 @@ while True:
  data = ser.readline().decode('utf-8').strip()
  if data.startswith("Detected Gesture: "):
      gesture = data.split(": ")[1]
- if gesture == "Gesture 1":
+ if gesture == "The sign is up":
  # Perform an action for Gesture 1
      print("Action for Gesture 1")
- elif gesture == "Gesture 2":
+ elif gesture == "The sign is down":
  # Perform an action for Gesture 2
      print("Action for Gesture 2")
  # Add more gesture actions as needed
